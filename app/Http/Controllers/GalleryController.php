334dce5efb84e8ec01e48313d335f85525b4b5a3
Igor <igorperic996@gmail.com>
2024-03-03 22:06:41 +0100

    GalleryController

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Gallery;
 use Illuminate\Http\Request;
 
 class GalleryController extends Controller
@@ -11,7 +12,8 @@ class GalleryController extends Controller
      */
     public function index()
     {
-        //
+        $galleries = Gallery::with(['images', 'user', 'comments'])->get();
+        return response()->json($galleries);
     }
 
     /**
@@ -19,30 +21,54 @@ class GalleryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'description' => 'nullable|string',
+        ]);
+
+        $validated['user_id'] = auth()->id();
+        $gallery = Gallery::create($validated);
+
+        return response()->json($gallery, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Gallery $gallery)
     {
-        //
+        return response()->json($gallery->load(['images', 'comments', 'user']));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Gallery $gallery)
     {
-        //
+        if($gallery->user_id !== auth()->id()) {
+            return response()->json(['message' => 'Ne mozete mijenjati ovu Galeriju'], 403);
+        }
+
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'description' => 'nullable|string',
+        ]);
+
+        $gallery->update($validated);
+
+        return response()->json($gallery);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Gallery $gallery)
     {
-        //
+        if($gallery->user_id !== auth()->id()) {
+            return response()->json(['message' => 'Ne mozete izbrisati ovu Galeriju'], 403);
+        }
+
+        $gallery->delete();
+        return response()->json(['message' => 'Galerija je izbrisana']);
     }
 }
