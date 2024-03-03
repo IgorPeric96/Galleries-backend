ff59d6b41cecbe472a5dd1e78e945a798a808d62
Igor <igorperic996@gmail.com>
2024-03-03 01:38:47 +0100

    auth-prvi-pokusaj

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+
+        //validacija
+        $validatedData = $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|confirmed|min:8|regex:/[0-9]/', //u zadatku se nije trazilo hash-ovanje tak da nisam stavljao
+            'terms' => 'accepted',
+        ]);
+
+        //kreiranje user-a
+        $user = User::create([
+            'first_name' => $validatedData['first_name'],
+            'last_name' => $validatedData['last_name'],
+            'email' => $validatedData['email'],
+            'password' => $validatedData['password'],
+        ]);
+
+        //ovim se registrovani korisnik odma uloguje 
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user,
+        ]);
+    }
+
+    public function login(Request $request) {
+        $validatedData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        
+        $user = User::where('email', $validatedData['email'])->first();
+        if($user && $validatedData['password'] === $user->password) {
+            $token = $user->createToken('authToken')->plainTextToken;
+
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'user' => $user,
+            ]);
+        } else {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+    }
+}
